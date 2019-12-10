3906eff39c6d7e51322a68c737f945c78955c53a
Ibrahim AHMED BACHA <fi_ahmed_bacha@esi.dz>
2019-12-10 23:47:44 +0100

    remove unused env variables

--- src/api/common/config/env.config.js
+++ src/api/common/config/env.config.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "PORT": process.env.PORT,
-    "APP_ENDPOINT": process.env.APP_ENDPOINT,
-    "API_ENDPOINT": process.env.API_ENDPOINT,
     "JWT_SECRET": process.env.JWT_SECRET,
-    "JWT_EXPIRATION_IN_SECONDS": process.env.JWT_EXPIRATION_IN_SECONDS,
-    "ENVIRONMENT": process.env.ENVIRONMENT,
     "PERMISSION_LEVELS": {
         "NORMAL_USER": process.env.NORMAL_USER,
         "PAID_USER": process.env.PAID_USER,
